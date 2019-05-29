277805f1cc84213e2962bb6d211a81ad90a6ded2
Sporadic <pino.bakker@gmail.com>
2019-05-29 21:55:01 +0200

    actually just remove this

--- src/main/kotlin/me/lotc/chat/user/Chatter.kt
+++ src/main/kotlin/me/lotc/chat/user/Chatter.kt
@@ -37,7 +37,6 @@ class Chatter(player: Player) {
 
     val channels = ChannelData(uuid, lock)
     val channel get() = channels.channel
-    val subscribedChannels get() = channels.subscribedChannels as Collection<Channel>
 
     val continuity = StringBuilder()
     val focus = Focus(uuid)
