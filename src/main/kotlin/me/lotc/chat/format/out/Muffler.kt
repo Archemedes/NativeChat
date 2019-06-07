250402a984c48ac26c46c3825892d98c04102444
Sporadic <pino.bakker@gmail.com>
2019-06-07 20:14:33 +0200

    1.0.1 fix: - final version of the proper emote formatting - muffled text displays to mods correctly - config actually has the muffly boyye for q/w - w to 2

--- src/main/kotlin/me/lotc/chat/format/out/Muffler.kt
+++ src/main/kotlin/me/lotc/chat/format/out/Muffler.kt
@@ -4,6 +4,7 @@ import co.lotc.core.bukkit.util.ChatBuilder
 import me.lotc.chat.message.ComposedMessage
 import me.lotc.chat.user.player
 import net.md_5.bungee.api.ChatColor.*
+import net.md_5.bungee.chat.ComponentSerializer
 
 val CHAT_MUFFLED_REPLACEMENT = "$DARK_GRAY*muffled* "
 
@@ -11,13 +12,14 @@ class Muffler(val act: Boolean, val modPex: String): OutFormatter {
     override fun format(message: ComposedMessage) {
         if(!act) return
         val speaks = message.sender.player ?: return
-        val listens = message.sender.player?: return
+        val listens = message.receiver.player?: return
 
         if(!speaks.hasLineOfSight(listens)){
             val cb = ChatBuilder(CHAT_MUFFLED_REPLACEMENT)
             if(listens.hasPermission(modPex)){
-                cb.insertion(message.getLegacyContent())
-                cb.hover("${BLUE}Peek$GRAY at the message\n{$GRAY}For moderation purposes only")
+                val json = ComponentSerializer.toString(*message.content)
+                cb.command("/tellraw @s $json")
+                cb.hover("${BLUE}Peek$GRAY at the message\n${GRAY}For moderation purposes only")
             }
 
             message.replaceContent(cb.build())
